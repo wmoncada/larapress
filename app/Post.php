79a18d1b42c31da0f23a5202e3242a5f0f6a9697
Walberto Moncada <monposmetal@gmail.com>
2021-08-07 11:45:29 -0600

    Configuracion de slugs para modelo Post

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    //
+    use Sluggable;
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true,
+            ]
+        ];
+    }
 }
